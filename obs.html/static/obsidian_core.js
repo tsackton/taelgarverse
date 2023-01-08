f2bdb95cde2ac105f4d69ab41ca247b26201efd7
Tim Sackton <tsackton@g.harvard.edu>
2023-01-08 12:17:16 -0500

    autopublish website

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/taelgarverse";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '72535625121190994820106534654115305448'                       // used to check whether the localStorage data is stale
+var gzip_hash = '240881436621596950244307398825703089262'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
